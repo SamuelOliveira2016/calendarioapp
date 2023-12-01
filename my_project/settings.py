6453ccb307d97b87ac83e5d3e1fb682f9780e896
SamuelOliveira2016 <samu.olive14@gmail.com>
2023-12-01 20:15:06 -0300

    Add Heroku domain to ALLOWED_HOSTS

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9r28rl*=&v8**agfl9--egk9@!(qclyx@6@=eez!*8-1jofch!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
